2880ef5cf09735e7faa9961bf562ed2a42590a5f
Anubhav Gain <iamanubhavgain@gmail.com>
2024-01-08 02:24:20 +0530

    idle loader/spinner class

--- sv.py
+++ sv.py
@@ -175,3 +175,59 @@ def logo():
     """
     print(logo_ascii)
     print(bcolors.ENDC)
+
+
+# Initiliazing the idle loader/spinner class
+class Spinner:
+    busy = False
+    delay = 0.005 # 0.05
+
+    @staticmethod
+    def spinning_cursor():
+        while 1:
+            #for cursor in '|/-\\/': yield cursor #←↑↓→
+            #for cursor in '←↑↓→': yield cursor
+            #for cursor in '....scanning...please..wait....': yield cursor
+            for cursor in ' ': yield cursor
+    def __init__(self, delay=None):
+        self.spinner_generator = self.spinning_cursor()
+        if delay and float(delay): self.delay = delay
+        self.disabled = False
+
+    def spinner_task(self):
+        inc = 0
+        try:
+            while self.busy:
+                if not self.disabled:
+                    x = bcolors.BG_SCAN_TXT_START+next(self.spinner_generator)+bcolors.BG_SCAN_TXT_END
+                    inc = inc + 1
+                    print(x,end='')
+                    if inc>random.uniform(0,terminal_size()): #30 init
+                        print(end="\r")
+                        bcolors.BG_SCAN_TXT_START = '\x1b[6;30;'+str(round(random.uniform(40,47)))+'m'
+                        inc = 0
+                    sys.stdout.flush()
+                time.sleep(self.delay)
+                if not self.disabled:
+                    sys.stdout.flush()
+
+        except (KeyboardInterrupt, SystemExit):
+            print("\n\t"+ bcolors.BG_ERR_TXT+"RapidScan received a series of Ctrl+C hits. Quitting..." +bcolors.ENDC)
+            sys.exit(1)
+
+    def start(self):
+        self.busy = True
+        try:
+            threading.Thread(target=self.spinner_task).start()
+        except Exception as e:
+            print("\n")
+        
+    def stop(self):
+        try:
+            self.busy = False
+            time.sleep(self.delay)
+        except (KeyboardInterrupt, SystemExit):
+            print("\n\t"+ bcolors.BG_ERR_TXT+"RapidScan received a series of Ctrl+C hits. Quitting..." +bcolors.ENDC)
+            sys.exit(1)
+
+# End ofloader/spinner class
