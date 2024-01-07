ccb0b2f93212efecd60e6a2e9d3dcb86a2495208
Anubhav Gain <iamanubhavgain@gmail.com>
2024-01-08 02:12:29 +0530

    Initializing the color module class

--- sv.py
+++ sv.py
@@ -72,3 +72,28 @@ def check_internet():
         val = 0
     os.system('rm rs_net > /dev/null 2>&1')
     return val
+
+
+# Initializing the color module class
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    BADFAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+    BG_ERR_TXT  = '\033[41m' # For critical errors and crashes
+    BG_HEAD_TXT = '\033[100m'
+    BG_ENDL_TXT = '\033[46m'
+    BG_CRIT_TXT = '\033[45m'
+    BG_HIGH_TXT = '\033[41m'
+    BG_MED_TXT  = '\033[43m'
+    BG_LOW_TXT  = '\033[44m'
+    BG_INFO_TXT = '\033[42m'
+
+    BG_SCAN_TXT_START = '\x1b[6;30;42m'
+    BG_SCAN_TXT_END   = '\x1b[0m'
+
