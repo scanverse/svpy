fea308f8daf6f154f7e8582a4c10a68d6799c822
Anubhav Gain <iamanubhavgain@gmail.com>
2024-01-08 02:09:55 +0530

    Started

---
+++ sv.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#   ____                   __     __                 
+#  / ___|  ___ __ _ _ __   \ \   / /__ _ __ ___  ___ 
+#  \___ \ / __/ _` | '_ \   \ \ / / _ \ '__/ __|/ _ \
+#   ___) | (_| (_| | | | |   \ V /  __/ |  \__ \  __/
+#  |____/ \___\__,_|_| |_|    \_/ \___|_|  |___/\___|
+#                                                    
+# Author     : Anubhav Gain
+# Tool       : ScanVerse
+# Usage      : python3 sv.py example.com
+# Description: This scanner automates the process of security scanning by using a
+#              multitude of available linux security tools and some custom scripts.
+#
