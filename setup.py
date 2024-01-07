d014be9cf2632bd9a903ac6ad16349141e4f026c
Anubhav Gain <iamanubhavgain@gmail.com>
2024-01-08 02:38:17 +0530

    setup added

--- setup.py
+++ setup.py
@@ -10,13 +10,13 @@ HERE = pathlib.Path(__file__).parent
 README = (HERE / "README.md").read_text()
 
 setup(
-    name                =   "rapidscan",
-    version             =   '1.2',
+    name                =   "scanverse",
+    version             =   '2.1',
     description         =   "The Multi-Tool Web Vulnerability Scanner.",
     long_description    =   README,
     long_description_content_type = "text/markdown",
-    url                 =   "https://github.com/skavngr/rapidscan",
-    author              =   "sh4nx0r",
+    url                 =   "https://github.com/scanverse/svpy",
+    author              =   "mranv",
     py_modules          =   ['rapidscan',],
     install_requires    =   [],
     python_requires=">=3.6",
